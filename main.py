ae9d15a66d574e8c416d73959b4df29a827e7e50
null-dimension <4hrishi4@gmail.com>
2021-10-18 12:20:53 +0530

    Set min and max charge variables

--- main.py
+++ main.py
@@ -2,22 +2,27 @@ import requests
 from bs4 import BeautifulSoup
 from win10toast import ToastNotifier
 import time
-# import logging
 
+# For debugging:
+
+# import logging
 # import http.client
 # http.client.HTTPConnection.debuglevel = 1
-
 # logging.basicConfig()
 # logging.getLogger().setLevel(logging.DEBUG)
 # requests_log = logging.getLogger("requests.packages.urllib3")
 # requests_log.setLevel(logging.DEBUG)
 # requests_log.propagate = True
-
-
-#headers = {"User-Agent": "Mozilla/5.0 (X11; CrOS x86_64 12871.102.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.141 Safari/537.36"}
+# headers = {"User-Agent": "Mozilla/5.0 (X11; CrOS x86_64 12871.102.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.141 Safari/537.36"}
 
 start_time = time.time()
 total_time = 0
+
+# Set the minimum charge to notify
+MIN_CHARGE = 40
+
+# Set the maximum charge to notify
+MAX_CHARGE = 95
 
 while True:
     try:
@@ -33,9 +38,9 @@ while True:
 
         print("Current charge is %s %%" % battery_percent)
         print("Battery is currently %s" % charging_status)
-        if battery_percent >= 95 and charging_status == "Charging":
+        if battery_percent >= MAX_CHARGE and charging_status == "Charging":
             toaster.show_toast("Battery is almost full!", "%s %% charged" % battery_percent, duration=30)
-        elif battery_percent <= 40 and charging_status == "Discharging":
+        elif battery_percent <= MIN_CHARGE and charging_status == "Discharging":
             toaster.show_toast("Battery needs charge!", "%s %% left" % battery_percent, duration=30)
         
         time.sleep(60.0 - ((time.time() - start_time) % 60.0))
